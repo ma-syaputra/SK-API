570ebba24e7f85d7d0120e191aed90dc620e9e2c
Putra <raydenkasyaputra@gmail.com>
2019-04-19 18:39:16 +0700

    p

--- controllers/dishes.js
+++ controllers/dishes.js
@@ -6,17 +6,17 @@ var res_app = require('./../response/status')
 exports.list = (req, res) => {
     const dataDishes = m_dishes.list()
     dataDishes.then(response => {
-        res_app.success_app(response,res);
+        res_app.success_app(response,res)
     }).catch(err => {
-        res_app.fail_app(err,res);
+        res_app.fail_app(err,res)
     })
 }
 exports.add = (req, res) => {
     const m_inc = m_inc_dishes.inc_dish()
     var dishes = req.body.name
-    if (!dishes) return res_app.bad_app(res);
+    if (!dishes) return res_app.bad_app(res)
     m_inc.then(response => {
-        console.log(response);
+        console.log(response)
         var data = {
             dishesId: response,
             dishesName : dishes,
@@ -24,25 +24,25 @@ exports.add = (req, res) => {
             isActive : 1}
             const dataDishes = m_dishes.save(data)
             dataDishes.then(response => {
-                res_app.send_app(response,res);
+                res_app.send_app(response,res)
             }).catch(err => {
-                res_app.fail_app(err,res);
+                res_app.fail_app(err,res)
             })
         }).catch(err => {
-            res_app.fail_app(err,res);
+            res_app.fail_app(err,res)
         })
     }
     exports.detail = (req, res) => {
         const id = parseInt(req.params.id);
-        const dataDishes = m_dishes.detail(id);
+        const dataDishes = m_dishes.detail(id)
         dataDishes.then(response => {
-            res_app.success_app(response,res);
+            res_app.success_app(response,res)
         }).catch(err => {
             res_app.fail_app(err,res);
         })
     }
     exports.edit = (req, res) => {
-        const id = parseInt(req.params.id);
+        const id = parseInt(req.params.id)
         var dishes = req.body.name
         if (!dishes) return res_app.bad_app(res)
         var data = {dishesName : dishes}
