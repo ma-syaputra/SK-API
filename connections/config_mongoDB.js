ec624ef02eb832af28a4f4a31465631a8e6cf982
Putra <raydenkasyaputra@gmail.com>
2019-04-20 20:23:22 +0700

    p

--- connections/config_mongoDB.js
+++ connections/config_mongoDB.js
@@ -2,6 +2,6 @@ var MongoClient = require('mongodb').MongoClient
 module.exports = {
     url: "mongodb://localhost:27017/m_dishes",
     db: "m_dishes",
-    collectionDB : "c_dishes",
+    collectionDB : "dishes",
     mDb : MongoClient
 }
