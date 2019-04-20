bea43e14a45962b16595f2fd1bd679ff076e51b4
Putra <raydenkasyaputra@gmail.com>
2019-04-20 17:33:31 +0700

    p

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express')
-var app = express();
+var cors = require('cors')
+var app = express()
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 var routeDishes = require('./routes/dishRoute');
